d0cfd3a11306cf7b880ad79f23897e8b979d51e5
bigeppo <carlog91cast@gmail.com>
2022-07-05 15:52:24 +0200

    add corr on line 45

--- js/script.js
+++ js/script.js
@@ -2,45 +2,54 @@
 const app = new Vue(
     {
         el: '#app',
+        data: {
 
-        slides: [
-            {
-                image: 'img/01.jpg',
-                title: 'Svezia',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+
+
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ],
+            // inizializzo la variabile che mi salva l'elemento dell'array
+            imageScrolled: 0,
+
+        },
+
+
+
+        // creo una funzione per capire se gli elementi degli array sono selezionati e inserire le immagini dinamicamente le immagini nel dom con VUE attraverso un method
+
+        methods: {
+            isImageScrolled: function (index) {
+                return (index == this.isImageScrolled) ? "isImageScrolled" : "nonImageScrolled";
             },
-            {
-                image: 'img/02.jpg',
-                title: 'Svizzera',
-                text: 'Lorem ipsum.',
-            },
-            {
-                image: 'img/03.jpg',
-                title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-                image: 'img/04.jpg',
-                title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-            },
-            {
-                image: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            addImage: function () {
+                
             }
-        ],
-        imageScrolled: 0,
+
+        },
     },
-    // inizializzo la variabile che mi salva l'elemento dell'array
-
-    // creo una funzione per capire se gli elementi degli array sono selezionati e inserire le immagini dinamicamente le immagini nel dom con VUE attraverso un method
-
-    methods: {
-    isImageScrolled: function (index) {
-        return (index = this.isImageScrolled) ? "isImageScrolled" : "nonImageScrolled";
-    },
-
-},
-
 );
